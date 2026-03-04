fa296a84852907a1a4b677d10a70465f0d31ea50
Rokavolgy <206435176+Rokavolgy@users.noreply.github.com>
2026-03-04 06:16:03 +0100

    Update prepare-gcc-15-minimal.sh

--- prepare-gcc-15-minimal.sh
+++ prepare-gcc-15-minimal.sh
@@ -7,12 +7,13 @@ apt install -y \
   libmpfr-dev \
   libmpc-dev \
   libisl-dev \
-  texinfo
+  texinfo \
+  lld
 
 wget https://ftp.gnu.org/gnu/gcc/gcc-15.2.0/gcc-15.2.0.tar.xz
 tar -xf gcc-15.2.0.tar.xz
 cd gcc-15.2.0
-
+export LDFLAGS="-fuse-ld=lld"
 ./contrib/download_prerequisites
 
 mkdir build
@@ -21,6 +22,7 @@ cd build
 ../configure \
   --enable-languages=c,c++ \
   --disable-multilib \
+  --disable-bootstrap \
   --prefix=/opt/gcc-15
 
 make -j$(nproc)
