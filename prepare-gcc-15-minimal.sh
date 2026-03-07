aec32a0c0aa4f41c987f09b6104aecf72c303d7a
Rokavolgy <206435176+Rokavolgy@users.noreply.github.com>
2026-03-07 20:28:28 +0100

    Update prepare-gcc-15-minimal.sh

--- prepare-gcc-15-minimal.sh
+++ prepare-gcc-15-minimal.sh
@@ -1,5 +1,6 @@
 apt update
 apt install -y \
+  --no-install-recommends \
   build-essential \
   wget \
   flex bison \
@@ -22,9 +23,33 @@ cd build
 ../configure \
   --enable-languages=c,c++ \
   --disable-multilib \
-  --disable-bootstrap \
   --prefix=/opt/gcc-15
 
 make -j$(nproc)
 
 make install
+
+export PATH=/opt/gcc-15/bin:$HOME/.local/bin:$PATH
+export PREFIX=/usr/local
+export PKG_CONFIG_PATH="$PREFIX/lib64/pkgconfig:$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CPPFLAGS="-I$PREFIX/include -I/usr/include $CPPFLAGS"
+export CFLAGS="-I$PREFIX/include -I/usr/include -O3 -mavx2 $CFLAGS"
+export LDFLAGS="-L$PREFIX/lib64 -L$PREFIX/lib -static -static-libgcc -static-libstdc++ $LDFLAGS"
+export LD_LIBRARY_PATH="$PREFIX/lib64:$PREFIX/lib:$LD_LIBRARY_PATH"
+
+
+echo "Building autoconf 2.71"
+apt install build-essential m4 perl texinfo
+
+wget https://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.gz
+tar -xf autoconf-2.71.tar.gz
+cd autoconf-2.71
+
+./configure
+make -j$(nproc)
+make install
+
+echo "deleting unneceserry files..."
+rm -rf /work
+
+apt-get autoremove -y
